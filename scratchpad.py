d4d47371a304347e2418bacbbfbc4c543738d54c
bkonersman <bkonersman@users.noreply.github.com>
2014-12-28 11:23:47 -0600

    for testing code blocks

---
+++ scratchpad.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python2.6
+import string
+
+game = 'hello'
+print game
+x =  string.capitalize(game)
+#string.format(game) = c
+#print x
+#print hello
+
+game.encode() = 23
+
+#exec('string(game)') = 23
+print hello
