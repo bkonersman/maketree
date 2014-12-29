406c4b8ef586d96fa0fc2c5cadc2c0ee9ff54547
Bill Konersman <bill.konersman@gmail.com>
2014-12-29 22:01:25 +0000

    added new files

---
+++ git_test.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+import string
+
+game = 'hello'
+print game
+x =  string.capitalize(game)
+#string.format(game) = c
+#print x
+#print hello
+
+g = game.encode()
+print x
+#exec('string(game)') = 23
